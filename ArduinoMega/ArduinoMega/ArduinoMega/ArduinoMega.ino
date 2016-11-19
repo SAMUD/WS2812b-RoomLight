50940c3644467dc76eb73a303c619f08648f9067
samu <werbungspam@outlook.com>
2016-11-19 15:29:55 +0100

    Initial commit

---
+++ ArduinoMega/ArduinoMega/ArduinoMega/ArduinoMega.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		ArduinoMega.ino
+ Created:	19.11.2016 15:25:02
+ Author:	sdaur
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
---
+++ ArduinoProMini/ArduinoProMini/ArduinoProMini.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		ArduinoProMini.ino
+ Created:	19.11.2016 15:25:33
+ Author:	sdaur
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
